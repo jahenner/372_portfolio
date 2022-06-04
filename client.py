7b56f30f45c55fc6f0ebb8f8df20e11a41be7b7a
Alex Henner <jahenner@gmail.com>
2022-06-04 15:30:24 -0400

    tic tac toe works, but need win conditions checked

--- client.py
+++ client.py
@@ -1,16 +1,35 @@
 from socket import *
+from server import print_board, STARTER, check_input, update_board
 serverName = "127.0.0.1"
 serverPort = 982
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-print(f"Connected to: localhost on port: {serverPort}\nType /q to quit\nEnter message to send...")
+print(f"Connected to: localhost on port: {serverPort}\nType /q to quit or /t to play tic-tac-toe\nEnter message to send...")
 client_message = input(">")
 clientSocket.send(client_message.encode())
 while client_message != "/q":
     if client_message == "/t":
-            print("Select a square you are o")
-            while client_message != "/q":
-                pass  
+        print_board(STARTER)
+        board = clientSocket.recv(1024).decode()
+        client_message = input("Select a square you are o or type /q to quit back to chat\n>")
+        
+        while client_message != "/q":
+            while not check_input(client_message, board):
+                client_message = input("Please select a valid input\n>")
+            clientSocket.send(client_message.encode())
+            print_board(update_board(board, "o", int(client_message)))
+            
+            board = clientSocket.recv(1024).decode()
+            if board == "/q":
+                print("Server ended game")
+                client_message = input("Enter message to send...\n>")
+                break
+            print_board(board)
+            
+            client_message = input("Select a square you are o or type /q to quit back to chat\n>")
+            
+        clientSocket.send(client_message.encode())
+        
     server_message = clientSocket.recv(1024).decode()
     print(server_message)
     client_message = input(">")
--- server.py
+++ server.py
@@ -1,48 +1,89 @@
 from socket import *
 
-
+STARTER = "123456789"
+    
 def print_board(board):
     result = "-------\n"
-    for row in board:
-        result += "|"
-        for col in row:
-            result += f"{col}|"
-        result += "\n-------\n"
+    for i in range(len(board)):
+        if i % 3 == 0:
+            result += "|"
+        result += f"{board[i]}|" if board[i] != "_" else " |"
+        if i % 3 == 2:
+            result += "\n-------\n"
     print(result)
-
-# create server environment
-serverName = '127.0.0.1'
-serverPort = 982
-serverSocket = socket(AF_INET, SOCK_STREAM)
-
-# bind the socket to the server environment
-serverSocket.bind((serverName, serverPort))
-
-# listen set to only allow 1 connection at a time
-serverSocket.listen(1)
-print(f'Server listening on: localhost port: {serverPort}')
-board = [[' ']*3]*3
-starter = []
-
-print_board(board)
-# make the process always on
-while True:
-    # allow clients to connect and create new socket to send data
-    connectionSocket, addr = serverSocket.accept()
-    print(f"Connected by: {addr}\nWaiting on a message...")
-    client_message = connectionSocket.recv(1024).decode()
-    print("Type /q to quit or /t for tic-tac-toe\nEnter message to send...")
-    while client_message != "/q":
-        if client_message == "/t":
-            tic_tac_toe_message = "Select a square. You will play as x.\n\n"
-            
-            connectionSocket.send(tic_tac_toe_message.encode())
-            while client_message != "/q":
-                pass        
-        print(client_message)
-        server_message = input(">")
-        connectionSocket.send(server_message.encode())
-        client_message = connectionSocket.recv(1024).decode()
     
-    print("Connection Closed")
-    connectionSocket.close()
+def update_board(board, player, location):
+    board = board[:(location-1)] + player + board[location:]
+    return board
+    
+def check_input(selection, board) -> bool:
+    try:
+        selection = int(selection)
+        if 0 < selection <= 9 and board[selection-1] == "_":
+            return True
+        else:
+            return False
+    except TypeError:
+        return False
+
+def main():
+    # create server environment
+    serverName = '127.0.0.1'
+    serverPort = 982
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+
+    # bind the socket to the server environment
+    serverSocket.bind((serverName, serverPort))
+
+    # listen set to only allow 1 connection at a time
+    serverSocket.listen(1)
+    print(f'Server listening on: localhost port: {serverPort}')
+
+    # make the process always on
+    while True:
+        # allow clients to connect and create new socket to send data
+        connectionSocket, addr = serverSocket.accept()
+        print(f"Connected by: {addr}\nWaiting on a message...")
+        client_message = connectionSocket.recv(1024).decode()
+        print("Type /q to quit or /t for tic-tac-toe\nEnter message to send...")
+        while client_message != "/q":
+            
+            if client_message == "/t":
+                board = "_"*9
+                connectionSocket.send(board.encode())
+                print_board(STARTER)
+                client_play = connectionSocket.recv(1024).decode()
+                board = update_board(board, "o", int(client_play))
+                print_board(board)
+                server_message = input("Select a square or type /q to go back to chat. You will play as x.\n>")
+                
+                while server_message != "/q":
+                    while not check_input(server_message, board):
+                        server_message = input("Please select a valid input\n>")
+                    board = update_board(board, "x", int(server_message))
+                    print_board(board)
+                    connectionSocket.send(board.encode())
+                    client_play = connectionSocket.recv(1024).decode()
+                    
+                    if client_play == "/q":
+                        print("Client ended game")
+                        server_message = input("Enter message to send...\n>")
+                        break
+                    
+                    board = update_board(board, "o", int(client_play))
+                    print_board(board)
+                    server_message = input("Select a square or type /q to go back to chat. You will play as x.\n>")
+                    
+                connectionSocket.send(server_message.encode())
+                client_message = connectionSocket.recv(1024).decode()
+                
+            print(client_message)
+            server_message = input(">")
+            connectionSocket.send(server_message.encode())
+            client_message = connectionSocket.recv(1024).decode()
+        
+        print("Connection Closed")
+        connectionSocket.close()
+        
+if __name__ == "__main__":
+    main()
