72c51b9a10688a6b38aefd470747b830019c5ce5
jahenner <jahenner@gmail.com>
2022-06-04 16:50:23 -0400

    Finished comments

--- client.py
+++ client.py
@@ -1,19 +1,33 @@
+# Alex Henner
+# CS372
+# Portfolio Project
+# 6/4/22
+# Sources: Computer Networking a Top Down Approach 7th edition Kurose, Ross
+
 from socket import *
 from server import print_board, STARTER, check_input, update_board
+
+# Server info
 serverName = "127.0.0.1"
 serverPort = 982
+
+# Client TCP connection
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
+
 print(f"Connected to: localhost on port: {serverPort}\nType /q to quit or /t to play tic-tac-toe\nEnter message to send...")
 client_message = input(">")
 clientSocket.send(client_message.encode())
 while client_message != "/q":
+    # client wants to play a game :)
     if client_message == "/t":
+        # print locations and receive blank board from server.
         print_board(STARTER)
         board = clientSocket.recv(1024).decode()
         client_message = input("Select a square you are o or type /q to quit back to chat\n>")
         
         while client_message != "/q":
+            # Check to make sure selection is valid
             while not check_input(client_message, board):
                 client_message = input("Please select a valid input\n>")
             clientSocket.send(client_message.encode())
@@ -24,6 +38,7 @@ while client_message != "/q":
                 print("Server ended game")
                 client_message = input("Enter message to send...\n>")
                 break
+            # After a win server sends back a message that starts with /end followed by winner X or O followed by board that won
             elif board[:4] == "/end":
                 winner = board[4]
                 print_board(board[5:])
@@ -40,7 +55,8 @@ while client_message != "/q":
             print_board(board)
             
             client_message = input("Select a square you are o or type /q to quit back to chat\n>")
-            
+        
+        # clean up to be able to go back to chat smoothly    
         clientSocket.send(client_message.encode())
         
     server_message = clientSocket.recv(1024).decode()
@@ -48,4 +64,5 @@ while client_message != "/q":
     client_message = input(">")
     clientSocket.send(client_message.encode())
 
+# Close connection
 clientSocket.close()
--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+# Alex Henner
+# CS372
+# Portfolio Project
+# 6/4/22
+# Sources: Computer Networking a Top Down Approach 7th edition Kurose, Ross
+
 from socket import *
 
 STARTER = "123456789"
@@ -72,20 +78,25 @@ def main():
         while client_message != "/q":
             
             if client_message == "/t":
+                # Create a blank board and send it to client
                 board = "_"*9
                 connectionSocket.send(board.encode())
                 print_board(STARTER)
+                
+                # Update clients pick and show to server
                 client_play = connectionSocket.recv(1024).decode()
                 board = update_board(board, "o", int(client_play))
                 print_board(board)
                 server_message = input("Select a square or type /q to go back to chat. You will play as x.\n>")
                 
                 while server_message != "/q":
+                    # Check move for validity and update board for server
                     while not check_input(server_message, board):
                         server_message = input("Please select a valid input\n>")
                     board = update_board(board, "x", int(server_message))
                     print_board(board)
                     
+                    # Check to see if someone has won
                     is_win = check_win(board)
                     if is_win is not None:
                         if is_win == "X":
@@ -100,6 +111,7 @@ def main():
                     connectionSocket.send(board.encode())
                     client_play = connectionSocket.recv(1024).decode()
                     
+                    # client doesn't want to play anymore :(
                     if client_play == "/q":
                         print("Client ended game")
                         server_message = input("Enter message to send...\n>")
@@ -108,6 +120,7 @@ def main():
                     board = update_board(board, "o", int(client_play))
                     print_board(board)
                     
+                    # game was updated so check again for winner
                     is_win = check_win(board)
                     if is_win is not None:
                         if is_win == "X":
@@ -121,14 +134,18 @@ def main():
                     
                     server_message = input("Select a square or type /q to go back to chat. You will play as x.\n>")
                 
+                # smooth transition back to chat
                 connectionSocket.send(server_message.encode())
                 client_message = connectionSocket.recv(1024).decode()
+                # client quit chat as well 
+                if client_message == "/q": break
                 
             print(client_message)
             server_message = input(">")
             connectionSocket.send(server_message.encode())
             client_message = connectionSocket.recv(1024).decode()
         
+        # client connection closed and waiting for next client
         print("Connection Closed")
         connectionSocket.close()
         
